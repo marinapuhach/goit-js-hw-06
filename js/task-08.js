a2a4791d194a9ad4239de1778415dd13543eda9a
marinapuhach <pugach.marina77@gmail.com>
2022-02-15 18:34:15 +0200

    corrections_2

--- js/task-08.js
+++ js/task-08.js
@@ -2,9 +2,12 @@ const formRef = document.querySelector('form.login-form')
 formRef.addEventListener('submit', (event) => {
   event.preventDefault();
   const formElements = event.currentTarget.elements;
-  if (formElements.email.value && formElements.password.value) {
-    console.log({ email, password })
-      } else {
-      alert('Please complete all fields')
+  if (formElements.email.value === '' || formElements.password.value  === '') {
+    alert('Please complete all fields')
     }
+    else {
+    console.log(formElements.email.value, formElements.password.value)
+  }
+    formRef.reset();
 });
+  
